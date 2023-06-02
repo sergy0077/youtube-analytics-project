6db0732ab16c2c2bdf9889d0e77d55d307b9ce5f
Sergy007 <kutsenkosergey@mail.ru>
2023-06-02 13:42:15 +0200

    Значение assert str(video2) == 'Moscow Python № 78' Иначе код работает с ошибкой

--- homework-4/main.py
+++ homework-4/main.py
@@ -5,4 +5,4 @@ if __name__ == '__main__':
     video1 = Video('AWX4JnAnjBE')  # 'AWX4JnAnjBE' - это id видео из ютуб
     video2 = PLVideo('4fObz_qw9u4', 'PLv_zOGKKxVph_8g2Mqc3LMhj0M_BfasbC')
     assert str(video1) == 'GIL в Python: зачем он нужен и как с этим жить'
-    assert str(video2) == 'MoscowPython Meetup 78 - вступление'
+    assert str(video2) == 'Moscow Python № 78'
