aa88d57627aae5e7420acab7de2c1d9658faab8f
Sergy007 <kutsenkosergey@mail.ru>
2023-06-02 13:40:15 +0200

    Доработаны тесты, с учетом изменений файла video.py

--- tests/test_video.py
+++ tests/test_video.py
@@ -7,8 +7,6 @@ def test_video_attributes():
     assert video.id == 'AWX4JnAnjBE'
     assert video.title == 'GIL в Python: зачем он нужен и как с этим жить'
     assert video.link == 'https://youtube.com/watch?v=AWX4JnAnjBE'
-    assert video.views == 1000
-    assert video.likes == 500
 
 
 def test_video_string_representation():
@@ -19,16 +17,14 @@ def test_video_string_representation():
 def test_plvideo_attributes():
     plvideo = PLVideo('4fObz_qw9u4', 'PLv_zOGKKxVph_8g2Mqc3LMhj0M_BfasbC')
     assert plvideo.id == '4fObz_qw9u4'
-    assert plvideo.title == 'MoscowPython Meetup 78 - вступление'
+    assert plvideo.title == 'Moscow Python № 78'
     assert plvideo.link == 'https://youtube.com/watch?v=4fObz_qw9u4'
-    assert plvideo.views == 1000
-    assert plvideo.likes == 500
     assert plvideo.playlist_id == 'PLv_zOGKKxVph_8g2Mqc3LMhj0M_BfasbC'
 
 
 def test_plvideo_string_representation():
     plvideo = PLVideo('4fObz_qw9u4', 'PLv_zOGKKxVph_8g2Mqc3LMhj0M_BfasbC')
-    assert str(plvideo) == 'MoscowPython Meetup 78 - вступление'
+    assert str(plvideo) == 'Moscow Python № 78'
 
 
 if __name__ == "__main__":
