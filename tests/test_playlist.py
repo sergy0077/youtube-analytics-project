de2201d29ba6ebbb8581b3d2ab8a02f304c4d08a
Sergy007 <kutsenkosergey@mail.ru>
2023-06-07 00:43:41 +0300

    В соответствии с изменениями основного кода доработан тест test_show_best_video

--- tests/test_playlist.py
+++ tests/test_playlist.py
@@ -1,11 +1,15 @@
-import pytest
 from datetime import timedelta
+import pytest
 from src.playlist import PlayList
 
 
 @pytest.fixture
 def playlist():
     return PlayList('PLv_zOGKKxVpj-n2qLkEM2Hj96LO6uqgQw')
+
+
+def test_playlist_id_initialization(playlist):
+    assert playlist.playlist_id == 'PLv_zOGKKxVpj-n2qLkEM2Hj96LO6uqgQw'
 
 
 def test_playlist_info(playlist):
@@ -19,7 +23,26 @@ def test_total_duration(playlist):
     assert playlist.total_duration.total_seconds() == 6592.0
 
 
-def test_show_best_video(playlist):
+def test_show_best_video(playlist, monkeypatch):
+    mock_playlist_items = {
+        'items': [
+            {
+                'id': 'cUGyMzWQcGM',
+                'statistics': {'likeCount': 10}
+            },
+            {
+                'id': 'video2',
+                'statistics': {'likeCount': 5}
+            },
+            {
+                'id': 'video3',
+                'statistics': {'likeCount': 8}
+            }
+        ]
+    }
+
+    monkeypatch.setattr(playlist, 'playlist_items', mock_playlist_items)
+
     assert playlist.show_best_video() == "https://youtu.be/cUGyMzWQcGM"
 
 
