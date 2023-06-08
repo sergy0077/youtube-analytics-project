32e10e0e6e6ad3161ef2224a1af1acf2a93eb41b
Sergy007 <kutsenkosergey@mail.ru>
2023-06-08 19:11:31 +0300

    в код класса Video вставлена конструкция  try/except. Если пользователь передал id, с которым невозможно получить данные о видео по API, то у экземпляра инициализируется только свойство video_id, а остальные поля принимают значение None. Код в файле main.py выдает ожидаемые значения по тестам.

--- src/video.py
+++ src/video.py
@@ -1,17 +1,26 @@
 from googleapiclient.discovery import build
 import os
 
+
 class Video:
     def __init__(self, video_id):
-        # Получение данных о видео из API
-        video_data = self._get_video_data(video_id)
+        try:
+            # Получение данных о видео из API
+            video_data = self._get_video_data(video_id)
 
-        # Инициализация атрибутов экземпляра класса Video
-        self.id = video_id
-        self.title = video_data['title']
-        self.link = f'https://youtube.com/watch?v={video_id}'
-        self.views = video_data['views']
-        self.likes = video_data['likes']
+            # Инициализация атрибутов экземпляра класса Video
+            self.id = video_id
+            self.title = video_data['title']
+            self.link = f'https://youtube.com/watch?v={video_id}'
+            self.views = video_data['views']
+            self.like_count = video_data['like_count']
+        except IndexError:
+            # В случае ошибки при получении данных, инициализировать только video_id
+            self.id = video_id
+            self.title = None
+            self.link = None
+            self.views = None
+            self.like_count = None
 
     def __str__(self):
         return self.title
@@ -38,7 +47,8 @@ class Video:
         return {
             'title': video_data['title'],
             'views': statistics['viewCount'],
-            'likes': statistics['likeCount']
+            'like_count': statistics['likeCount'],
+
         }
 
 
